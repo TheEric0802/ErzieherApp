94db8d9644a425811199cbc04f2f8bfd936788bd
Eric Schmidt <eric0802@web.de>
2025-05-23 15:25:47 +0200

    add auth in backend

---
+++ backend/src/main/java/casa/eric/erzieherapp/backend/security/AuthController.java
@@ -0,0 +1,21 @@
+package casa.eric.erzieherapp.backend.security;
+
+import casa.eric.erzieherapp.backend.model.AppUserDTO;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @GetMapping
+    public AppUserDTO getMe(@AuthenticationPrincipal OAuth2User user) {
+        return AppUserDTO.builder()
+                .id(user.getName())
+                .username(user.getAttributes().get("preferred_username").toString())
+                .build();
+    }
+}
---
+++ backend/src/main/java/casa/eric/erzieherapp/backend/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package casa.eric.erzieherapp.backend.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration @EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${app.url}")
+    private String appUrl;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/api/auth").authenticated()
+                .anyRequest().permitAll())
+                .oauth2Login(oauth2 -> oauth2
+                    .defaultSuccessUrl(appUrl));
+
+        return http.build();
+    }
+}
