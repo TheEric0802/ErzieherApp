4099359d0782d5b9d845b8dab00a9f6a329adf6e
Eric Schmidt <eric0802@web.de>
2025-05-23 15:50:29 +0200

    add auth in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useCallback, useEffect, useState } from "react";
+import axios from "axios";
+import type { appUser } from "./types/appUser.ts";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const host: string =
+    window.location.host === "localhost:5173"
+      ? "http://localhost:8080"
+      : window.location.origin;
+
+  const [appUser, setAppUser] = useState<appUser | null | undefined>(undefined);
+
+  const loadUser = useCallback(() => {
+    axios
+      .get<appUser>("api/auth")
+      .then((r) => {
+        if (r.data.username) {
+          setAppUser(r.data);
+        } else {
+          setAppUser(null);
+        }
+      })
+      .catch(() => setAppUser(null));
+  }, []);
+
+  useEffect(() => {
+    loadUser();
+  }, [loadUser]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {appUser ? (
+        <>
+          <p>Logged in as {appUser.username}</p>
+          <button onClick={() => window.open(host + "/logout", "_self")}>
+            Logout
+          </button>
+        </>
+      ) : (
+        <>
+          <p>Not logged in</p>
+          <button
+            onClick={() =>
+              window.open(host + "/oauth2/authorization/authentik", "_self")
+            }
+          >
+            Login
+          </button>
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
