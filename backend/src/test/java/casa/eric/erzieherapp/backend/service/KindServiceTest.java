f90c5dfe2f15e99e1210f52ac40721ba75f7a46e
Eric Schmidt <eric0802@web.de>
2025-06-05 13:47:25 +0200

    remove unused code

--- backend/src/test/java/casa/eric/erzieherapp/backend/service/KindServiceTest.java
+++ backend/src/test/java/casa/eric/erzieherapp/backend/service/KindServiceTest.java
@@ -2,7 +2,6 @@ package casa.eric.erzieherapp.backend.service;
 
 import casa.eric.erzieherapp.backend.model.Gruppe;
 import casa.eric.erzieherapp.backend.model.Kind;
-import casa.eric.erzieherapp.backend.model.KindDTO;
 import casa.eric.erzieherapp.backend.repository.KindRepository;
 import org.junit.jupiter.api.Test;
 
