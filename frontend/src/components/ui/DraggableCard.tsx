17056e9345c91598f00e0601930347b8c905cde5
Eric Schmidt <eric0802@web.de>
2025-06-16 10:54:43 +0200

    add drag and drop to set Anwesenheit of Kind in Frontend

---
+++ frontend/src/components/ui/DraggableCard.tsx
@@ -0,0 +1,42 @@
+import { useDraggable } from "@dnd-kit/core";
+import Card, { type CardProps } from "./Card.tsx";
+import * as React from "react";
+
+type DraggableCardProps = {
+  children?: React.ReactNode;
+  cardProps: CardProps;
+  draggableId: string;
+  draggableData?: object;
+};
+
+export default function DraggableCard({
+  children = null,
+  cardProps,
+  draggableId,
+  draggableData,
+}: DraggableCardProps) {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: draggableId,
+    data: draggableData,
+  });
+
+  const style = transform
+    ? {
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+      }
+    : undefined;
+
+  return (
+    <div
+      ref={setNodeRef}
+      {...attributes}
+      {...listeners}
+      style={style}
+      className={"flex-grow"}
+    >
+      <Card {...cardProps} cardRef={setNodeRef}>
+        {children}
+      </Card>
+    </div>
+  );
+}
---
+++ frontend/src/components/ui/DroppableCard.tsx
@@ -0,0 +1,25 @@
+import { useDroppable } from "@dnd-kit/core";
+import Card, { type CardProps } from "./Card.tsx";
+import * as React from "react";
+
+type DroppableCardProps = {
+  children?: React.ReactNode;
+  cardProps: CardProps;
+  droppableId: string;
+};
+
+export default function DroppableCard({
+  children = null,
+  cardProps,
+  droppableId,
+}: DroppableCardProps) {
+  const { setNodeRef } = useDroppable({
+    id: droppableId,
+  });
+
+  return (
+    <div ref={setNodeRef} className={"flex-grow"}>
+      <Card {...cardProps}>{children}</Card>
+    </div>
+  );
+}
